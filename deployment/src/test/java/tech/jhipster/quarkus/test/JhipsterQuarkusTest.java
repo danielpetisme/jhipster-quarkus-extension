429c13d5306985c09b45d84279cfb7df42fb22bc
danielpetisme <daniel.petisme@gmail.com>
2021-01-08 22:22:55 +0100

    Ready to dev

--- deployment/src/test/java/tech/jhipster/quarkus/test/JhipsterQuarkusTest.java
+++ deployment/src/test/java/tech/jhipster/quarkus/test/JhipsterQuarkusTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
 
-class JhipsterQuarkusTest {
+public class JhipsterQuarkusTest {
 
     @RegisterExtension
     static final QuarkusUnitTest unitTest = new QuarkusUnitTest() // Start unit test with your extension loaded
